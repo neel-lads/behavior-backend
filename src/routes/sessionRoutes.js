ea3e677b7dcf99599a18c11ba637c3a7b03e5382
neel-lads <neel.lads2004@gmail.com>
2026-02-28 18:15:19 +0000

    enable ssl for render postgres

--- src/routes/sessionRoutes.js
+++ src/routes/sessionRoutes.js
@@ -4,7 +4,7 @@ import { createSession, logEvent } from '../controllers/sessionController.js';
 
 const router = express.Router();
 
-router.post('/', authenticate, createSession);
+router.post('/create', authenticate, createSession);
 router.post('/event', authenticate, logEvent);
 
 export default router;
