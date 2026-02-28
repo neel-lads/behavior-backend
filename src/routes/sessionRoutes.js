f7d0a03650c144bfa21e64895023210c5d18d274
neel-lads <neel.lads2004@gmail.com>
2026-02-28 17:35:48 +0000

    fix middleware import in sessionRoutes

--- src/routes/sessionRoutes.js
+++ src/routes/sessionRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { authenticate } from '../middleware/auth.js';
+import { authenticate } from '../middleware/authMiddleware.js';
 import { createSession, logEvent } from '../controllers/sessionController.js';
 
 const router = express.Router();
